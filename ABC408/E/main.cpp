609823266e1a04568e06c6aae6e3b381c9648470
BioBIOS <biobios1024@gmail.com>
2025-05-31 23:04:55 +0900

    clear: ABC408 E

--- ABC408/E/main.cpp
+++ ABC408/E/main.cpp
@@ -31,31 +31,46 @@ int main()
         graph[v].emplace_back(u, w);
     }
     
-    set<pair<uint64_t, size_t>> pq; // (cost, node)
-    vector<uint64_t> min_cost(N, UINT64_MAX);
-    min_cost[N - 1] = 0;
-    pq.insert({0, N - 1}); // Start from node N - 1 with cost 0
-
-    while (!pq.empty())
-    {
-        auto [cost, node] = *pq.begin();
-        pq.erase(pq.begin());
+    uint64_t mask = (0b1 << 30) - 1;
+    
+    for (size_t i = 0; i < 30; ++i){
+        vector<bool> visited(N, false);
+        bool found = false;
         
-        if (cost > min_cost[node])
-            continue; // Skip if we already found a better cost
+        mask ^= (0b1 << (29 - i));
         
-        for (const auto& [neighbor, weight] : graph[node])
+        queue<size_t> q;
+        q.push(0);
+        visited[0] = true;
+        while(!q.empty() && !found)
         {
-            uint64_t new_cost = cost | weight;
-            if (new_cost < min_cost[neighbor])
+            size_t current = q.front();
+            q.pop();
+
+            for (const auto& [neighbor, weight] : graph[current])
             {
-                pq.erase({min_cost[neighbor], neighbor});
-                min_cost[neighbor] = new_cost;
-                pq.insert({new_cost, neighbor});
+                if ((weight & mask) != weight)
+                    continue;
+                
+                if (neighbor == N - 1)
+                {
+                    found = true;
+                    break;
+                }
+                
+                if (!visited[neighbor])
+                {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
             }
         }
+        
+        if (!found)
+            mask ^= (0b1 << (29 - i));
     }
     
-    cout << min_cost[0] << endl; // Output the minimum cost to reach node N-1
+    cout << mask << endl;
+    
     return 0;
 }
