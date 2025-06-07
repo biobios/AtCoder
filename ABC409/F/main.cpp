dd39df3507a9131b6701ae61ccd6b43c79a2e6f8
BioBIOS <biobios1024@gmail.com>
2025-06-08 00:27:37 +0900

    save: ABC409 F incomplete answer

--- ABC409/F/main.cpp
+++ ABC409/F/main.cpp
@@ -19,7 +19,6 @@ struct Union_Find
 {
     vector<int64_t> parent;
     vector<int64_t> rank;
-    map<size_t, set<size_t>> group_map;
 
     Union_Find(size_t size)
         : parent(size), rank(size, 0)
@@ -27,7 +26,6 @@ struct Union_Find
         for (size_t i = 0; i < size; ++i)
         {
             parent[i] = i;
-            group_map[i].insert(i);
         }
     }
 
@@ -40,7 +38,7 @@ struct Union_Find
         return parent[x];
     }
 
-    void unite(int64_t x, int64_t y)
+    bool unite(int64_t x, int64_t y)
     {
         int64_t root_x = find(x);
         int64_t root_y = find(y);
@@ -50,30 +48,26 @@ struct Union_Find
             if (rank[root_x] < rank[root_y])
             {
                 parent[root_x] = root_y;
-                group_map[root_y].merge(group_map[root_x]);
-                group_map.erase(root_x);
             }
             else if (rank[root_x] > rank[root_y])
             {
                 parent[root_y] = root_x;
-                group_map[root_x].merge(group_map[root_y]);
-                group_map.erase(root_y);
             }
             else
             {
                 parent[root_y] = root_x;
                 rank[root_x]++;
-                group_map[root_x].merge(group_map[root_y]);
-                group_map.erase(root_y);
             }
+            return true;
         }
+        
+        return false;
     }
 
     void add_elem()
     {
         parent.push_back(parent.size());
         rank.push_back(0);
-        group_map[parent.size() - 1].insert(parent.size() - 1);
     }
 };
 
@@ -88,7 +82,16 @@ int main()
     }
     
     Union_Find uf(N);
-
+    
+    map<uint64_t, vector<pair<size_t, size_t>>> distance_map;
+    for (size_t i = 0; i < N; ++i)
+    {
+        for (size_t j = i + 1; j < N; ++j)
+        {
+            uint64_t distance = abs((int64_t)(nodes[i].first - nodes[j].first)) + abs((int64_t)(nodes[i].second - nodes[j].second));
+            distance_map[distance].emplace_back(i, j);
+        }
+    }
     
     for (size_t i = 0; i < Q; ++i)
     {
@@ -99,54 +102,43 @@ int main()
             uint64_t a, b;
             cin >> a >> b;
             uf.add_elem();
+            for (size_t j = 0; j < nodes.size(); ++j)
+            {
+                const auto& [x, y] = nodes[j];
+                uint64_t distance = abs((int64_t)(x - a)) + abs((int64_t)(y - b));
+                distance_map[distance].emplace_back(j, nodes.size());
+            }
             nodes.push_back({a, b});
         }else if(q_type == 2)
         {
-            if (uf.group_map.size() < 2)
-            {
-                cout << "-1" << endl;
-                continue;
-            }
-            vector<pair<uint64_t, uint64_t>> min_distance_nodes;
-            uint64_t min_distance = UINT64_MAX;
-            for (auto it_i = uf.group_map.begin(); it_i != uf.group_map.end(); ++it_i)
-            {
-                for (auto it_j = next(it_i); it_j != uf.group_map.end(); ++it_j)
+            while(true){
+                if (distance_map.empty())
                 {
-                    uint64_t min_distance_i_j = UINT64_MAX;
-                    size_t i = it_i->first;
-                    size_t j = it_j->first;
-                    for (const auto& elem_i : it_i->second)
+                    cout << -1 << endl;
+                    break;
+                }
+
+                auto& [distance, pairs] = *distance_map.begin();
+                bool merged = false;
+                for (const auto& [u, v] : pairs)
+                {
+                    if (uf.unite(u, v))
                     {
-                        for (const auto& elem_j : it_j->second)
-                        {
-                            uint64_t distance = abs((int64_t)(nodes[elem_i].first - nodes[elem_j].first)) + abs((int64_t)(nodes[elem_i].second - nodes[elem_j].second));
-                            if (distance < min_distance_i_j)
-                            {
-                                min_distance_i_j = distance;
-                            }
-                        }
-                    }
-                    
-                    if (min_distance_i_j < min_distance)
-                    {
-                        min_distance = min_distance_i_j;
-                        min_distance_nodes.clear();
-                        min_distance_nodes.push_back({i, j});
-                    }
-                    else if (min_distance_i_j == min_distance)
-                    {
-                        min_distance_nodes.push_back({i, j});
+                        merged = true;
                     }
                 }
+                
+                if (merged)
+                {
+                    cout << distance << endl;
+                    distance_map.erase(distance);
+                    break;
+                }else
+                {
+                    distance_map.erase(distance);
+                    continue;
+                }
             }
-            
-            for (const auto& [i, j] : min_distance_nodes)
-            {
-                uf.unite(i, j);
-            }
-            
-            cout << min_distance << endl;
         }else
         {
             uint64_t u, v;
